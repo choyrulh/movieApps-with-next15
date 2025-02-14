ab832d318afe925d8abf50b7a0c10aa1e3303eb4
choyrulh <choirul.humam@students.amikom.ac.id>
2025-02-14 14:49:10 +0700

    Update page.tsx

--- app/watch-list/page.tsx
+++ app/watch-list/page.tsx
@@ -119,7 +119,7 @@ const MovieGrid = ({ items, removeFromWatchlist }: any) => (
     className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6"
   >
     <AnimatePresence>
-      {items.map((item) => (
+      {items.map((item: any) => (
         <motion.div
           key={item.id}
           layout
@@ -174,7 +174,7 @@ const TVShowGrid = ({ items, removeFromWatchlist }: any) => (
     className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6"
   >
     <AnimatePresence>
-      {items.map((item) => (
+      {items.map((item: any) => (
         <motion.div
           key={item.id}
           layout
@@ -229,7 +229,7 @@ const ActorGrid = ({ items, removeFromWatchlist }: any) => (
     className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6"
   >
     <AnimatePresence>
-      {items.map((item) => (
+      {items.map((item: any) => (
         <motion.div
           key={item.id}
           layout
